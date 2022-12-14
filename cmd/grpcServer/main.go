08c996ff6be0854eb02a111302965155e62a820e
paulodutra <paulodutra.info@gmail.com>
2022-12-14 11:28:42 -0300

    implementing grpc server

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/paulodutra/golang-grpc/internal/database"
+	"github.com/paulodutra/golang-grpc/internal/pb"
+	"github.com/paulodutra/golang-grpc/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	listen, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Server(listen); err != nil {
+		panic(err)
+	}
+
+}
